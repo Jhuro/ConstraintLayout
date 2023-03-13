dd972414d11f0fa3d24c21dd3cea7cf5e5be7a59
jupbc <jupbcan100@hotmail.com>
2023-03-12 23:56:02 -0500

    Lab

--- app/src/main/java/co/edu/unipiloto/constraintlayout/Correo.java
+++ app/src/main/java/co/edu/unipiloto/constraintlayout/Correo.java
@@ -1,7 +1,11 @@
 package co.edu.unipiloto.constraintlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Correo extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class Correo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_correo);
+        Button send = (Button) findViewById(R.id.send);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                siguienteActividad();
+            }
+        });
+    }
+
+    private void siguienteActividad(){
+
+        Intent intent = new Intent(this, PublicarArticulo.class);
+        startActivity(intent);
     }
 }
