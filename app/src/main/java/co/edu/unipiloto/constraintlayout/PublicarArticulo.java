3835afa7d3d973c1278856442de7e43e020bd16a
jupbc <jupbcan100@hotmail.com>
2023-03-12 23:56:23 -0500

    Lab

---
+++ app/src/main/java/co/edu/unipiloto/constraintlayout/PublicarArticulo.java
@@ -0,0 +1,17 @@
+package co.edu.unipiloto.constraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class PublicarArticulo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publicar_articulo);
+    }
+}
